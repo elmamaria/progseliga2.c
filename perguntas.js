1f6ceaab36b0a6fb28478d4cc1482cd0f01e0d6d
elmamaria <161838447+elmamaria@users.noreply.github.com>
2024-12-06 11:12:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'biologia',
+    'qual o menor animal do mundo?',
+    'O PlÂncton'
+)
+
+criaCartao(
+    ' matematica',
+    'Qual a raiz quadrada de pi?',
+    '1,7724'
+)
+
+criaCartao(
+    'medicina',
+    'qual orgão responsavel por limpar o sangue?',
+    ' Os rins'
+)
+
+criaCartao(
+    'quimica',
+    'qual formula do cal?',
+    ' CaO'
+)
